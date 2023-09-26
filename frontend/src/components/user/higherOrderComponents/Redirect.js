8ff2679a08c6ca2e727c5d042d92a4cb24d7f432
md arif <arifahd92@gmail.com>
2023-09-26 14:11:23 +0530

    implemented protected routing

---
+++ frontend/src/components/user/higherOrderComponents/Redirect.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+export default function Redirect({ Component }) {
+  const navigate = useNavigate();
+  const userToken = localStorage.getItem("userToken");
+  const StringId = localStorage.getItem("userId");
+  const email = localStorage.getItem("userEmail");
+  let id;
+
+  if (StringId) {
+    id = JSON.parse(StringId);
+  }
+
+  useEffect(() => {
+    if (userToken && email && id) {
+      verify();
+    }
+  }, []);
+  //verify function
+  const verify = async () => {
+    try {
+      const token = userToken;
+
+      const apiUrl = "http://localhost:4000/verify-user";
+      const requestData = {
+        id,
+        email,
+      };
+      const response = await axios.post(apiUrl, requestData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.data;
+      console.log(data);
+      if (data.verification) {
+        navigate(`/expense/${id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
